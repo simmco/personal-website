12a4c075a89f2ad50e427f059c24a6f1a3c6b33e
simmco <m.simmelbauer@gmail.com>
2020-09-26 18:38:38 -0700

    update on email success / errorr

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,22 +1,28 @@
 const sgMail = require('@sendgrid/mail');
 
 export default (req, res) => {
-  console.log(req.body);
   const body = JSON.parse(req.body);
-  console.log({ body });
   const { name, email, message } = body;
-  console.log({ name, email, message });
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.send({
-    from: email, // sender address
-    to: 'm.simmelbauer@gmail.com', // list of receivers
-    subject: `Nachricht von ${name} - ${email}`, // Subject line
-    text: 'ERFOLG', // plain text body
-    html: `<div>${message} <br /> ${name} - ${email}</div>`,
-  });
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ status: 'mail send!' }));
+  return sgMail
+    .send({
+      from: email, // sender address
+      to: 'm.simmelbauer@gmail.com', // list of receivers
+      subject: `Nachricht von ${name} - ${email}`, // Subject line
+      text: 'ERFOLG', // plain text body
+      html: `<div>${message} <br /> ${name} - ${email}</div>`,
+    })
+    .then(() => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ status: 'mail send!' }));
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ status: 'mail error!' }));
+    });
 };
